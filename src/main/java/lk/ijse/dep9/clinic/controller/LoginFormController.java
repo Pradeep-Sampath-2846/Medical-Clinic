d21410aaf5f71f747309cfbcd3bb9ce6379a80bd
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-09-02 13:17:41 +0530

    Close #2

--- src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
@@ -1,5 +1,15 @@
 package lk.ijse.dep9.clinic.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
 
+    public JFXTextField txtUsername;
+    public JFXTextField txtPassword;
+    public JFXButton btnLogin;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
